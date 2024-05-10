79dcb8fadd047e6a9e93c180f5f75536ed8bb384
Dunnatello <michaeldunn92001@gmail.com>
2024-05-10 13:39:15 -0500

    Added Health Bar UI to Zombies
    Added a health bar that appears above zombies once they are damaged to give the player feedback.

--- Assets/Game/Scripts/Bot/AI.cs
+++ Assets/Game/Scripts/Bot/AI.cs
@@ -1,8 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 using Visions.Player;
+using UnityEngine.UI;
 
 public class AI : MonoBehaviour {
 
@@ -13,8 +13,13 @@ public class AI : MonoBehaviour {
     private Health playerHealthScript;
 
     //Zombie Stats
-    [SerializeField] private int health = 100;
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float attackDelay = 1.0f;
+
+    [SerializeField] private GameObject healthUI;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private float updateHealthSpeed = 5f;
 
     private float delayRemaining;
     private float damageMultiplier;
@@ -32,10 +37,47 @@ public class AI : MonoBehaviour {
         //Randomize Movement Speed and Damage Multiplier
         agent.speed = Random.Range( 3.0f, 6.0f );
         damageMultiplier = Random.Range( 0.6f, 1.5f );
+
+        healthSlider.value = health / maxHealth;
+        healthUI.SetActive( false );
     }
 
     public void TakeDamage( int damage ) {
+        
         health -= damage;
+        UpdateUI( );
+
+    }
+
+    public void UpdateUI( ) {
+
+        if ( !healthUI.activeInHierarchy )
+            healthUI.SetActive( true );
+
+        StartCoroutine( TransitionHealth( ) );
+
+    }
+
+    private IEnumerator TransitionHealth( ) {
+
+        float visualHealth = healthSlider.value;
+        float targetHealth = health / maxHealth;
+
+        while ( healthSlider.value != targetHealth ) {
+
+            visualHealth -= Time.deltaTime * updateHealthSpeed;
+
+            if ( visualHealth < targetHealth ) {
+                
+                visualHealth = targetHealth;
+            }
+
+            healthSlider.value = visualHealth;
+
+            yield return null;
+
+        } 
+
     }
 
     private void Update( ) {
